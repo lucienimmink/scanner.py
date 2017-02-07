8fafd13e1a7551003ab057c6fc219fbf91858ec2
Lucien Immink <lucien@arielext.org>
2017-02-07 21:18:09 +0100

    [TASK] Add _flac postfix to ids

--- scanner.py
+++ scanner.py
@@ -89,10 +89,10 @@ class FlacTrack:
         except KeyError:
             self.disc = 1
         if idartist and self.album and self.title and (self.number is not None):
-            self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + str(self.number) + _force_unicode(self.title, 'utf-8')).encode('utf-8'))
+            self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + str(self.number) + _force_unicode(self.title, 'utf-8')).encode('utf-8') + '_flac')
         else: 
             if idartist and self.album and self.title:
-                self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + _force_unicode(self.title, 'utf-8')).encode('utf-8'))
+                self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + _force_unicode(self.title, 'utf-8')).encode('utf-8') + '_flac')
         self.modified = os.path.getmtime(os.path.split(path)[0]) * 1000
         self.type = 'flac'
     def time(self):
