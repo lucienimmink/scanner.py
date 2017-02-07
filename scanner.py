0d4f56181df3c365c2fc1db1556b49297583e960
Lucien Immink <lucien@arielext.org>
2017-02-07 20:57:31 +0100

    [TASK] Add flax support atm its hacky and very basic will make a better version soon I promise

--- scanner.py
+++ scanner.py
@@ -3,6 +3,7 @@
 """This program is made possible by the awesome python knowledge of my darling Ivana <3 for ever"""
 
 import os, fnmatch, json, sys, codecs, time, eyed3, argparse, logging, base64
+from mutagen.flac import FLAC, StreamInfo
 
 parser = argparse.ArgumentParser(description='Scans a given directory for MP3\'s and places the output file in an optional directory');
 parser.add_argument('scanpath', metavar='scanpath', help='directory to scan')
@@ -53,10 +54,51 @@ class Track:
             if idartist and self.album and self.title:
                 self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + _force_unicode(self.title, 'utf-8')).encode('utf-8'))
         self.modified = os.path.getmtime(os.path.split(path)[0]) * 1000
-
-
     def time(self):
         return self.duration
+
+class FlacTrack:
+    def __init__ (self, file, path):
+        idartist = ''
+        self.artist = file['artist'][0]
+        self.albumartist = None
+        try:
+            self.albumartist = file['albumartist'][0]
+        except KeyError:
+            self.albumartist = None
+
+        if self.artist is not None:
+            idartist = self.artist
+        if self.albumartist is not None:
+            idartist = self.albumartist
+        self.album = file['album'][0]
+        self.year = None
+        try:
+            self.year = file['date'][0]
+        except KeyError:
+            self.year = 0
+        self.number = file['tracknumber'][0]
+        self.title = file['title'][0]
+
+        self.duration = file.info.length * 1000
+        self.path = _force_unicode(path, "utf-8").replace("\\", "\\\\")
+        self.path = self.path[len(rootpath):]
+        self.disc = None
+        try:
+            self.disc = file['discnumber'][0]
+        except KeyError:
+            self.disc = 1
+        if idartist and self.album and self.title and (self.number is not None):
+            self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + str(self.number) + _force_unicode(self.title, 'utf-8')).encode('utf-8'))
+        else: 
+            if idartist and self.album and self.title:
+                self.id = base64.b64encode((_force_unicode(idartist, 'utf-8') + _force_unicode(self.album, 'utf-8') + _force_unicode(self.title, 'utf-8')).encode('utf-8'))
+        self.modified = os.path.getmtime(os.path.split(path)[0]) * 1000
+        self.type = 'flac'
+    def time(self):
+        return self.duration
+
+
 
 def find_files(directory, pattern):
     for root, dirs, files in os.walk(directory):
@@ -134,11 +176,45 @@ def parseFile(filename, jsonFile, showInfo=True):
                     sys.stdout.flush()
             jsonFile.append(json.dumps(track.__dict__,sort_keys=True, indent=2))
 
+def parseFlac(filename, jsonFile, showInfo=True):
+    global artists
+    global albums
+    global totalArtist
+    global totalAlbums
+    global totalTime
+    global nrScanned
+    
+    song = FLAC(filename)
+    if song is not None:
+        track = FlacTrack(song, filename)
+        nrScanned = nrScanned + 1
+        perc = int((float(float(nrScanned) / float(countfiles))) * 100)
+        p.seek(0)
+        p.write(str(perc))
+        p.truncate()
+        if (countfiles > 100 and nrScanned % int(countfiles/100) == 0 and showInfo):
+            inc = time.time()
+            #print "Scanner has scanned" , str(nrScanned) , "files, time elapsed =", ums(inc-start)
+            diff = inc-start
+            if (perc > 0):
+                tot = (diff / perc) * 100
+                eta = tot - diff
+                sys.stdout.write("" + str(perc) + "% done, ETA: " +  ums(eta, False) + "\r")
+                sys.stdout.flush()
+        jsonFile.append(json.dumps(track.__dict__,sort_keys=True, indent=2))
 allfiles = find_files(rootpath, '*.mp3')
+allfilesflac = find_files(rootpath, '*.flac')
 countfiles = sum(1 for e in allfiles)
-print("Starting scan for {0} mp3 files in '{1}'".format(countfiles, rootpath))
+countfiles += sum(1 for e in allfilesflac)
+
+print "Starting scan for {0} media files in '{1}'".format(countfiles, rootpath)
 for filename in find_files(rootpath, '*.mp3'):
     parseFile(filename, jsonFile)
+
+for filename in find_files(rootpath, '*.flac'):
+    parseFlac(filename, jsonFile)
+
+
 f.write("[" + ",\n".join(jsonFile) + "]")
 f.close()
 p.close()
